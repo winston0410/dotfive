e377512e9556eaf149b3f7f7f756e11869545c10
kghugo <hugosum.dev@protonmail.com>
2021-12-19 23:30:11 +0000

    Add notes for handling init state

--- test/src/channel.ts
+++ test/src/channel.ts
@@ -12,6 +12,8 @@ const mkChannel = () => {
             channel.addEventListener('message', (data: IAction<IData>) => {
                 reducer(data)
             });
+
+            // TODO Need to handle the init state with channel as well. Ideally the init data should be fetched through API. Handling this could be done by using actor model, and use IndexedDB/LocalStorage as the mailbox for actor
             
 			elector = createLeaderElection(channel);
 			await elector.awaitLeadership()
